bbaa49e4ca8b964b5c32a2874e505bacb1f9b65a
fawaz <fawazfadel@cmail.carleton.ca>
2024-12-08 13:35:04 -0500

    Added Date and File classes

---
+++ assignment22/File.cc
@@ -0,0 +1,22 @@
+#include "File.h"
+#include <iostream>
+File::File(const string& name, const string& content, Date& date) 
+    : name(name), content(content), lastModified(date) {}
+
+
+bool File::lessThan(File& other) {
+    return lastModified.lessThan(other.lastModified);
+}
+
+
+void File::print() {
+    cout << "File: " << name << endl;
+    cout << "Last modified: ";
+    lastModified.print();
+    cout << endl;
+}
+
+void File::printContents() {
+    print();
+    cout << "Content: " << content << endl;
+}
